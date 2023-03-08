6717ec15068ae3289c9fbef8d56041b6f1c4a981
FANT0M100 <giorgie103188@gmail.com>
2023-03-08 18:09:02 +0400

    init axios and get data

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,37 @@
 // Global app controller
+import * as searchView from './views/searchView'
+import { clerLoader, elements, renderLoader } from "./views/base";
+import Search from './models/search';
+
+
+const state = {};
+window.state = state;
+
+/* search controller */
+const contorlSearch = async () => {
+    //1)get query from view
+    const query = searchView.getInput();
+
+    if(query){
+        //2) new search object and add state
+        state.search = new Search(query);
+
+        //3)clear input value
+        searchView.clearInput();
+        //4)add loader
+        renderLoader(elements.searchResList);
+        
+        try {
+            await state.search.getResults();
+            //5)clear loader
+            clerLoader();
+        } catch (error) {
+            alert('Error Search');
+        }
+    }
+};
+
+elements.searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    contorlSearch();
+});
--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,3 +1,21 @@
 export const elements = {
     searchForm: document.querySelector('.search'),
-}
+    searchInput: document.querySelector('.search__field'),
+    searchResList: document.querySelector('.results__list'),
+};
+
+export const renderLoader = parent => {
+    const loader = `
+        <div class='loader'>
+           <svg>
+              <use href='img/icons.svg#icon-cw' ></use>
+           </svg>
+        </div>
+    `
+    parent.insertAdjacentHTML("afterbegin", loader);
+};
+
+export const clerLoader = () => {
+    const loader = document.querySelector('.loader');
+    loader && loader.parentElement.removeChild(loader);
+};
